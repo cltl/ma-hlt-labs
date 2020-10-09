1da174713ca2a041d74dcca3ffb1b12576440cfb
Selene Baez <s.baezsantamaria@vu.nl>
2020-10-10 00:28:45 +0200

    more info on failing to get messages in chatbot

--- lab4.chatbot/utils.py
+++ lab4.chatbot/utils.py
@@ -27,8 +27,15 @@ class BotHandler:
         method = 'getUpdates'
         params = {'timeout': timeout, 'offset': offset}
         resp = requests.get(self.api_url + method, params)
-
-        return resp.json()['result']
+        
+        if resp.status_code == 401:  
+            print("Error: Unauthorized access. Please check the Bot's token")
+        
+        if resp.status_code == 200:
+            if resp.json() and 'result' in resp.json():
+                return resp.json()['result']
+            else:
+                print("Error: No messages found")
 
     def filter_messages_by(self, update, chat_id):
         """ Function to filter messages by user id"""
@@ -37,13 +44,20 @@ class BotHandler:
     def get_last_message_by(self, chat_id):
         """ Function to get the last message sent to the bot by a specific user"""
         messages = self.get_all_messages()
-        messages_by_user = list(filter(lambda m: self.filter_messages_by(m, chat_id), messages))
+        if messages:
+            messages_by_user = list(filter(lambda m: self.filter_messages_by(m, chat_id), messages))
+            
+            if messages_by_user:
+                last_message = None
+                if messages_by_user and 'message' in messages_by_user[-1].keys():
+                    try:
+                        last_message = messages_by_user[-1]['message']['text']
+                    except:
+                        last_message = ''
 
-        last_message = None
-        if messages_by_user and 'message' in messages_by_user[-1].keys():
-            last_message = messages_by_user[-1]['message']['text']
-
-        return last_message
+                    return last_message
+            else:
+                print("Error: No messages by this user found")
 
     def send_message_to(self, chat_id, text):
         """ Function to send a message from the bot to a specific user"""

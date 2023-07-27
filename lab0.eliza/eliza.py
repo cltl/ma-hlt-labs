993fb6b20740940f2e8b5884475da51f1fb43f6b
piekvossen <piekvossen@gmail.com>
2023-07-27 11:59:44 +0200

    eliza

--- lab0.eliza/eliza.py
+++ lab0.eliza/eliza.py
@@ -1,9 +1,8 @@
 import re
 import random
 import eliza_language as lang
-import pandas as pd
+import json
 
-human_name='Stranger'
 system_name='Eliza'
 
 conversation=[]
@@ -26,27 +25,51 @@ def analyze(statement):
 
 def talk_to_me():
     print("My name is {}. What is your name?".format(system_name))
+    global human_name
     human_name=input("> ")
     turn_id = 0
-    prompt = "Hello {}. How are you feeling today?".format(human_name)
-    turn = {'utterance':prompt, 'speaker': system_name, 'turn_id':turn_id}
+    eliza_start = "Hello {}. How are you feeling today?".format(human_name)
+    turn = {'utterance':eliza_start, 'speaker': system_name, 'turn_id':turn_id}
     conversation.append(turn)
-    print(prompt)
+    print(eliza_start)
 
     while True:
-        statement = input("> ")
+        human_input = input("> ")
 
-        if statement.lower()=='stop' or statement.lower()== "quit" or statement.lower()== "bye":
-            break
         turn_id += 1   
-        turn = {'utterance':statement, 'speaker': human_name, 'turn_id':turn_id}
+        turn = {'utterance':human_input, 'speaker': human_name, 'turn_id':turn_id}
         conversation.append(turn)
+        if human_input.lower()=='stop' or human_input.lower()== "quit" or human_input.lower()== "bye":
+            break
+        else:
+            eliza_response = analyze(human_input)
+            turn = {'utterance':eliza_response, 'speaker': system_name, 'turn_id':turn_id}
+            conversation.append(turn)            
+            print(eliza_response)
+    return
 
-        prompt = analyze(statement)
-        turn = {'utterance':prompt, 'speaker': system_name, 'turn_id':turn_id}
-        conversation.append(turn)            
-        print(prompt)
+def save_to_json(filename = "chat_with_eliza.json"):
+    with open(human_name+"_"+filename,'w') as file:
+        json.dump(conversation, file, indent = 4)
 
+def print_chat():
+    for turn in conversation:
+        print(turn)
+
+def annotate_chat(labels=[]):
+    gold_labels = []
+    for turn in conversation:
+        gold = ""
+        speaker = turn['speaker']
+        utterance = turn['utterance']
+        print(speaker, ":", utterance)
+        if speaker=='Eliza':
+            gold='neutral'
+        else: 
+            ### we keep getting the user input till one of them matches a label
+            while not gold in labels:
+                gold = input("label> ")
+        turn['Gold']=gold
 
 if __name__ == "__main__":
     talk_to_me()
